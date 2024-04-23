3b91d00b9cbe37475782729193c9be36bc2921d1
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-04-23 14:56:37 +0700

    refactor: apply prettier on generated.ts file

--- frontend/src/graphql/generated.ts
+++ frontend/src/graphql/generated.ts
@@ -1,10 +1,16 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+import { gql } from "@apollo/client";
+import * as Apollo from "@apollo/client";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -18,138 +24,199 @@ export type Scalars = {
 };
 
 export type AgencyType = {
-  __typename?: 'AgencyType';
-  createdAt: Scalars['DateTime'];
-  id: Scalars['UUID'];
-  name: Scalars['String'];
-  updatedAt: Scalars['DateTime'];
+  __typename?: "AgencyType";
+  createdAt: Scalars["DateTime"];
+  id: Scalars["UUID"];
+  name: Scalars["String"];
+  updatedAt: Scalars["DateTime"];
 };
 
 export type AgencyUserType = {
-  __typename?: 'AgencyUserType';
+  __typename?: "AgencyUserType";
   agency?: Maybe<AgencyType>;
-  agencyId: Scalars['UUID'];
-  createdAt: Scalars['DateTime'];
-  id: Scalars['UUID'];
-  name: Scalars['String'];
-  updatedAt: Scalars['DateTime'];
-  userId: Scalars['UUID'];
+  agencyId: Scalars["UUID"];
+  createdAt: Scalars["DateTime"];
+  id: Scalars["UUID"];
+  name: Scalars["String"];
+  updatedAt: Scalars["DateTime"];
+  userId: Scalars["UUID"];
 };
 
 export type CreateAgency = {
-  __typename?: 'CreateAgency';
+  __typename?: "CreateAgency";
   agencyUser?: Maybe<AgencyUserType>;
 };
 
 export type CreateAgencyInput = {
   agencyUser: CreateAgencyUserInput;
-  name: Scalars['String'];
+  name: Scalars["String"];
 };
 
 export type CreateAgencyUserInput = {
-  name: Scalars['String'];
+  name: Scalars["String"];
 };
 
 export type CreateTalentProfile = {
-  __typename?: 'CreateTalentProfile';
+  __typename?: "CreateTalentProfile";
   talentProfile?: Maybe<TalentProfileType>;
 };
 
 export type CreateTalentProfileInput = {
-  avatar?: InputMaybe<Scalars['String']>;
-  bio?: InputMaybe<Scalars['String']>;
-  name: Scalars['String'];
+  avatar?: InputMaybe<Scalars["String"]>;
+  bio?: InputMaybe<Scalars["String"]>;
+  name: Scalars["String"];
 };
 
 export type Mutation = {
-  __typename?: 'Mutation';
+  __typename?: "Mutation";
   createAgency?: Maybe<CreateAgency>;
   createTalentProfile?: Maybe<CreateTalentProfile>;
 };
 
-
 export type MutationCreateAgencyArgs = {
   input: CreateAgencyInput;
 };
-
 
 export type MutationCreateTalentProfileArgs = {
   input: CreateTalentProfileInput;
 };
 
 export type Query = {
-  __typename?: 'Query';
+  __typename?: "Query";
   myAgencyUser?: Maybe<AgencyUserType>;
   talentProfiles?: Maybe<Array<Maybe<TalentProfileType>>>;
   user?: Maybe<UserType>;
 };
 
 export type TalentProfileType = {
-  __typename?: 'TalentProfileType';
-  avatar?: Maybe<Scalars['String']>;
-  bio?: Maybe<Scalars['String']>;
-  createdAt: Scalars['DateTime'];
-  id: Scalars['UUID'];
-  name: Scalars['String'];
-  updatedAt: Scalars['DateTime'];
+  __typename?: "TalentProfileType";
+  avatar?: Maybe<Scalars["String"]>;
+  bio?: Maybe<Scalars["String"]>;
+  createdAt: Scalars["DateTime"];
+  id: Scalars["UUID"];
+  name: Scalars["String"];
+  updatedAt: Scalars["DateTime"];
 };
 
 export type UserType = {
-  __typename?: 'UserType';
-  createdAt: Scalars['DateTime'];
-  email: Scalars['String'];
-  id: Scalars['UUID'];
-  updatedAt: Scalars['DateTime'];
+  __typename?: "UserType";
+  createdAt: Scalars["DateTime"];
+  email: Scalars["String"];
+  id: Scalars["UUID"];
+  updatedAt: Scalars["DateTime"];
 };
 
-export type MyAgencyUserQueryVariables = Exact<{ [key: string]: never; }>;
+export type MyAgencyUserQueryVariables = Exact<{ [key: string]: never }>;
 
-
-export type MyAgencyUserQuery = { __typename?: 'Query', myAgencyUser?: { __typename?: 'AgencyUserType', id: any, name: string, createdAt: any, updatedAt: any, agencyId: any, userId: any, agency?: { __typename?: 'AgencyType', id: any, name: string, createdAt: any, updatedAt: any } | null } | null };
+export type MyAgencyUserQuery = {
+  __typename?: "Query";
+  myAgencyUser?: {
+    __typename?: "AgencyUserType";
+    id: any;
+    name: string;
+    createdAt: any;
+    updatedAt: any;
+    agencyId: any;
+    userId: any;
+    agency?: {
+      __typename?: "AgencyType";
+      id: any;
+      name: string;
+      createdAt: any;
+      updatedAt: any;
+    } | null;
+  } | null;
+};
 
 export type CreateAgencyMutationVariables = Exact<{
   input: CreateAgencyInput;
 }>;
 
+export type CreateAgencyMutation = {
+  __typename?: "Mutation";
+  createAgency?: {
+    __typename?: "CreateAgency";
+    agencyUser?: {
+      __typename?: "AgencyUserType";
+      id: any;
+      name: string;
+      createdAt: any;
+      updatedAt: any;
+      agencyId: any;
+      userId: any;
+      agency?: {
+        __typename?: "AgencyType";
+        id: any;
+        name: string;
+        createdAt: any;
+        updatedAt: any;
+      } | null;
+    } | null;
+  } | null;
+};
 
-export type CreateAgencyMutation = { __typename?: 'Mutation', createAgency?: { __typename?: 'CreateAgency', agencyUser?: { __typename?: 'AgencyUserType', id: any, name: string, createdAt: any, updatedAt: any, agencyId: any, userId: any, agency?: { __typename?: 'AgencyType', id: any, name: string, createdAt: any, updatedAt: any } | null } | null } | null };
+export type TalentProfilesQueryVariables = Exact<{ [key: string]: never }>;
 
-export type TalentProfilesQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type TalentProfilesQuery = { __typename?: 'Query', talentProfiles?: Array<{ __typename?: 'TalentProfileType', id: any, name: string, createdAt: any, updatedAt: any } | null> | null };
+export type TalentProfilesQuery = {
+  __typename?: "Query";
+  talentProfiles?: Array<{
+    __typename?: "TalentProfileType";
+    id: any;
+    name: string;
+    createdAt: any;
+    updatedAt: any;
+  } | null> | null;
+};
 
 export type CreateTalentProfileMutationVariables = Exact<{
   input: CreateTalentProfileInput;
 }>;
 
+export type CreateTalentProfileMutation = {
+  __typename?: "Mutation";
+  createTalentProfile?: {
+    __typename?: "CreateTalentProfile";
+    talentProfile?: {
+      __typename?: "TalentProfileType";
+      id: any;
+      name: string;
+      createdAt: any;
+      updatedAt: any;
+    } | null;
+  } | null;
+};
 
-export type CreateTalentProfileMutation = { __typename?: 'Mutation', createTalentProfile?: { __typename?: 'CreateTalentProfile', talentProfile?: { __typename?: 'TalentProfileType', id: any, name: string, createdAt: any, updatedAt: any } | null } | null };
+export type MeQueryVariables = Exact<{ [key: string]: never }>;
 
-export type MeQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type MeQuery = { __typename?: 'Query', user?: { __typename?: 'UserType', id: any, email: string, createdAt: any, updatedAt: any } | null };
-
+export type MeQuery = {
+  __typename?: "Query";
+  user?: {
+    __typename?: "UserType";
+    id: any;
+    email: string;
+    createdAt: any;
+    updatedAt: any;
+  } | null;
+};
 
 export const MyAgencyUserDocument = gql`
-    query myAgencyUser {
-  myAgencyUser {
-    id
-    name
-    createdAt
-    updatedAt
-    agencyId
-    userId
-    agency {
+  query myAgencyUser {
+    myAgencyUser {
       id
       name
       createdAt
       updatedAt
+      agencyId
+      userId
+      agency {
+        id
+        name
+        createdAt
+        updatedAt
+      }
     }
   }
-}
-    `;
+`;
 
 /**
  * __useMyAgencyUserQuery__
@@ -166,38 +233,64 @@ export const MyAgencyUserDocument = gql`
  *   },
  * });
  */
-export function useMyAgencyUserQuery(baseOptions?: Apollo.QueryHookOptions<MyAgencyUserQuery, MyAgencyUserQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<MyAgencyUserQuery, MyAgencyUserQueryVariables>(MyAgencyUserDocument, options);
-      }
-export function useMyAgencyUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MyAgencyUserQuery, MyAgencyUserQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<MyAgencyUserQuery, MyAgencyUserQueryVariables>(MyAgencyUserDocument, options);
-        }
-export type MyAgencyUserQueryHookResult = ReturnType<typeof useMyAgencyUserQuery>;
-export type MyAgencyUserLazyQueryHookResult = ReturnType<typeof useMyAgencyUserLazyQuery>;
-export type MyAgencyUserQueryResult = Apollo.QueryResult<MyAgencyUserQuery, MyAgencyUserQueryVariables>;
+export function useMyAgencyUserQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    MyAgencyUserQuery,
+    MyAgencyUserQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<MyAgencyUserQuery, MyAgencyUserQueryVariables>(
+    MyAgencyUserDocument,
+    options
+  );
+}
+export function useMyAgencyUserLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    MyAgencyUserQuery,
+    MyAgencyUserQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<MyAgencyUserQuery, MyAgencyUserQueryVariables>(
+    MyAgencyUserDocument,
+    options
+  );
+}
+export type MyAgencyUserQueryHookResult = ReturnType<
+  typeof useMyAgencyUserQuery
+>;
+export type MyAgencyUserLazyQueryHookResult = ReturnType<
+  typeof useMyAgencyUserLazyQuery
+>;
+export type MyAgencyUserQueryResult = Apollo.QueryResult<
+  MyAgencyUserQuery,
+  MyAgencyUserQueryVariables
+>;
 export const CreateAgencyDocument = gql`
-    mutation CreateAgency($input: CreateAgencyInput!) {
-  createAgency(input: $input) {
-    agencyUser {
-      id
-      name
-      createdAt
-      updatedAt
-      agencyId
-      userId
-      agency {
+  mutation CreateAgency($input: CreateAgencyInput!) {
+    createAgency(input: $input) {
+      agencyUser {
         id
         name
         createdAt
         updatedAt
+        agencyId
+        userId
+        agency {
+          id
+          name
+          createdAt
+          updatedAt
+        }
       }
     }
   }
-}
-    `;
-export type CreateAgencyMutationFn = Apollo.MutationFunction<CreateAgencyMutation, CreateAgencyMutationVariables>;
+`;
+export type CreateAgencyMutationFn = Apollo.MutationFunction<
+  CreateAgencyMutation,
+  CreateAgencyMutationVariables
+>;
 
 /**
  * __useCreateAgencyMutation__
@@ -216,23 +309,37 @@ export type CreateAgencyMutationFn = Apollo.MutationFunction<CreateAgencyMutatio
  *   },
  * });
  */
-export function useCreateAgencyMutation(baseOptions?: Apollo.MutationHookOptions<CreateAgencyMutation, CreateAgencyMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<CreateAgencyMutation, CreateAgencyMutationVariables>(CreateAgencyDocument, options);
-      }
-export type CreateAgencyMutationHookResult = ReturnType<typeof useCreateAgencyMutation>;
-export type CreateAgencyMutationResult = Apollo.MutationResult<CreateAgencyMutation>;
-export type CreateAgencyMutationOptions = Apollo.BaseMutationOptions<CreateAgencyMutation, CreateAgencyMutationVariables>;
-export const TalentProfilesDocument = gql`
-    query TalentProfiles {
-  talentProfiles {
-    id
-    name
-    createdAt
-    updatedAt
-  }
+export function useCreateAgencyMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    CreateAgencyMutation,
+    CreateAgencyMutationVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useMutation<
+    CreateAgencyMutation,
+    CreateAgencyMutationVariables
+  >(CreateAgencyDocument, options);
 }
-    `;
+export type CreateAgencyMutationHookResult = ReturnType<
+  typeof useCreateAgencyMutation
+>;
+export type CreateAgencyMutationResult =
+  Apollo.MutationResult<CreateAgencyMutation>;
+export type CreateAgencyMutationOptions = Apollo.BaseMutationOptions<
+  CreateAgencyMutation,
+  CreateAgencyMutationVariables
+>;
+export const TalentProfilesDocument = gql`
+  query TalentProfiles {
+    talentProfiles {
+      id
+      name
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 /**
  * __useTalentProfilesQuery__
@@ -249,30 +356,56 @@ export const TalentProfilesDocument = gql`
  *   },
  * });
  */
-export function useTalentProfilesQuery(baseOptions?: Apollo.QueryHookOptions<TalentProfilesQuery, TalentProfilesQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<TalentProfilesQuery, TalentProfilesQueryVariables>(TalentProfilesDocument, options);
-      }
-export function useTalentProfilesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<TalentProfilesQuery, TalentProfilesQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<TalentProfilesQuery, TalentProfilesQueryVariables>(TalentProfilesDocument, options);
-        }
-export type TalentProfilesQueryHookResult = ReturnType<typeof useTalentProfilesQuery>;
-export type TalentProfilesLazyQueryHookResult = ReturnType<typeof useTalentProfilesLazyQuery>;
-export type TalentProfilesQueryResult = Apollo.QueryResult<TalentProfilesQuery, TalentProfilesQueryVariables>;
+export function useTalentProfilesQuery(
+  baseOptions?: Apollo.QueryHookOptions<
+    TalentProfilesQuery,
+    TalentProfilesQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<TalentProfilesQuery, TalentProfilesQueryVariables>(
+    TalentProfilesDocument,
+    options
+  );
+}
+export function useTalentProfilesLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<
+    TalentProfilesQuery,
+    TalentProfilesQueryVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<TalentProfilesQuery, TalentProfilesQueryVariables>(
+    TalentProfilesDocument,
+    options
+  );
+}
+export type TalentProfilesQueryHookResult = ReturnType<
+  typeof useTalentProfilesQuery
+>;
+export type TalentProfilesLazyQueryHookResult = ReturnType<
+  typeof useTalentProfilesLazyQuery
+>;
+export type TalentProfilesQueryResult = Apollo.QueryResult<
+  TalentProfilesQuery,
+  TalentProfilesQueryVariables
+>;
 export const CreateTalentProfileDocument = gql`
-    mutation CreateTalentProfile($input: CreateTalentProfileInput!) {
-  createTalentProfile(input: $input) {
-    talentProfile {
-      id
-      name
-      createdAt
-      updatedAt
+  mutation CreateTalentProfile($input: CreateTalentProfileInput!) {
+    createTalentProfile(input: $input) {
+      talentProfile {
+        id
+        name
+        createdAt
+        updatedAt
+      }
     }
   }
-}
-    `;
-export type CreateTalentProfileMutationFn = Apollo.MutationFunction<CreateTalentProfileMutation, CreateTalentProfileMutationVariables>;
+`;
+export type CreateTalentProfileMutationFn = Apollo.MutationFunction<
+  CreateTalentProfileMutation,
+  CreateTalentProfileMutationVariables
+>;
 
 /**
  * __useCreateTalentProfileMutation__
@@ -291,23 +424,37 @@ export type CreateTalentProfileMutationFn = Apollo.MutationFunction<CreateTalent
  *   },
  * });
  */
-export function useCreateTalentProfileMutation(baseOptions?: Apollo.MutationHookOptions<CreateTalentProfileMutation, CreateTalentProfileMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<CreateTalentProfileMutation, CreateTalentProfileMutationVariables>(CreateTalentProfileDocument, options);
-      }
-export type CreateTalentProfileMutationHookResult = ReturnType<typeof useCreateTalentProfileMutation>;
-export type CreateTalentProfileMutationResult = Apollo.MutationResult<CreateTalentProfileMutation>;
-export type CreateTalentProfileMutationOptions = Apollo.BaseMutationOptions<CreateTalentProfileMutation, CreateTalentProfileMutationVariables>;
-export const MeDocument = gql`
-    query Me {
-  user {
-    id
-    email
-    createdAt
-    updatedAt
-  }
+export function useCreateTalentProfileMutation(
+  baseOptions?: Apollo.MutationHookOptions<
+    CreateTalentProfileMutation,
+    CreateTalentProfileMutationVariables
+  >
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useMutation<
+    CreateTalentProfileMutation,
+    CreateTalentProfileMutationVariables
+  >(CreateTalentProfileDocument, options);
 }
-    `;
+export type CreateTalentProfileMutationHookResult = ReturnType<
+  typeof useCreateTalentProfileMutation
+>;
+export type CreateTalentProfileMutationResult =
+  Apollo.MutationResult<CreateTalentProfileMutation>;
+export type CreateTalentProfileMutationOptions = Apollo.BaseMutationOptions<
+  CreateTalentProfileMutation,
+  CreateTalentProfileMutationVariables
+>;
+export const MeDocument = gql`
+  query Me {
+    user {
+      id
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
 
 /**
  * __useMeQuery__
@@ -324,14 +471,18 @@ export const MeDocument = gql`
  *   },
  * });
  */
-export function useMeQuery(baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
-      }
-export function useMeLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
-        }
+export function useMeQuery(
+  baseOptions?: Apollo.QueryHookOptions<MeQuery, MeQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+}
+export function useMeLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<MeQuery, MeQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<MeQuery, MeQueryVariables>(MeDocument, options);
+}
 export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
 export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
 export type MeQueryResult = Apollo.QueryResult<MeQuery, MeQueryVariables>;
