87aadad820bc97044318fc13ff2a01a62b7726ab
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-04-23 15:37:06 +0700

    refactor: rename my_agency_resolver to my_agency_user_resolver

--- api/app/graph/schema.py
+++ api/app/graph/schema.py
@@ -5,7 +5,7 @@ from app.graph.mutations.create_talent_profile import CreateTalentProfile
 
 from app.graph.resolvers.me_resolver import MeResolver
 from app.graph.resolvers.talent_profiles_resolver import TalentProfilesResolver
-from app.graph.resolvers.my_agency_resolver import MyAgencyUserResolver
+from app.graph.resolvers.my_agency_user_resolver import MyAgencyUserResolver
 
 
 
