a1a08af4cbb7cc336c141e818b90dd5bf577cd0e
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-04-23 12:18:58 +0700

    feat: add settings page for agency and agency user with sidebar

---
+++ frontend/src/components/pages/app/agencyUser/AgencyUser.tsx
@@ -0,0 +1,11 @@
+import AgencySettingsLayout from "@/components/layout/AgencySettingsLayout";
+
+const AgencyUser = () => {
+  return (
+    <AgencySettingsLayout>
+      <p>This is the agency user settings page</p>
+    </AgencySettingsLayout>
+  );
+};
+
+export default AgencyUser;
