7d60d2ab60065b9e8b1fa82d46f73473e097b2fd
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-04-25 15:38:14 +0700

    fix: frontend form

--- frontend/src/app/app/test_upload_file/page.tsx
+++ frontend/src/app/app/test_upload_file/page.tsx
@@ -113,7 +113,7 @@ const AppTopPage = () => {
           </div>
 
           {uploadedFileNames.length > 0 && (
-            <div className="mt-4 text-sm text-gray-600 px-6">
+            <div className="mb-4 text-sm text-gray-600 px-6">
               <strong className="block mb-2">Uploaded files:</strong>
               <ul className="space-y-2">
                 {uploadedFileNames.map((name, index) => (
@@ -137,7 +137,7 @@ const AppTopPage = () => {
 
         {/* Action buttons */}
         <div className="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6 flex flex-row justify-between">
-          <div className="flex items-center">
+          <div className="flex items-center max-w-[60%] sm:max-w-[75%] md:max-w-[80%] overflow-hidden">
             {message && (
               <div
                 className={`text-sm font-semibold ${
@@ -159,7 +159,7 @@ const AppTopPage = () => {
             <button
               type="button"
               disabled={uploadedFileNames.length === 0}
-              className={`inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${uploadedFileNames.length === 0 ? "opacity-50 cursor-not-allowed" : ""}`}
+              className={`inline-flex items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${uploadedFileNames.length === 0 ? "opacity-50 cursor-not-allowed" : ""}`}
               onClick={handleSubmitFile}
             >
               Create
