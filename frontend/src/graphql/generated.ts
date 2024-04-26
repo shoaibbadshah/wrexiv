dd4b654576dc5a201a6a1559da68c0777d5e9c09
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-04-26 11:31:14 +0700

    chore: change createAgency query response; update form interface

--- frontend/src/graphql/generated.ts
+++ frontend/src/graphql/generated.ts
@@ -44,7 +44,8 @@ export type AgencyUserType = {
 
 export type CreateAgency = {
   __typename?: "CreateAgency";
-  agencyUser?: Maybe<AgencyUserType>;
+  agency?: Maybe<AgencyType>;
+  success?: Maybe<Scalars["Boolean"]>;
 };
 
 export type CreateAgencyInput = {
@@ -136,22 +137,7 @@ export type CreateAgencyMutation = {
   __typename?: "Mutation";
   createAgency?: {
     __typename?: "CreateAgency";
-    agencyUser?: {
-      __typename?: "AgencyUserType";
-      id: any;
-      name: string;
-      createdAt: any;
-      updatedAt: any;
-      agencyId: any;
-      userId: any;
-      agency?: {
-        __typename?: "AgencyType";
-        id: any;
-        name: string;
-        createdAt: any;
-        updatedAt: any;
-      } | null;
-    } | null;
+    success?: boolean | null;
   } | null;
 };
 
@@ -270,20 +256,7 @@ export type MyAgencyUserQueryResult = Apollo.QueryResult<
 export const CreateAgencyDocument = gql`
   mutation CreateAgency($input: CreateAgencyInput!) {
     createAgency(input: $input) {
-      agencyUser {
-        id
-        name
-        createdAt
-        updatedAt
-        agencyId
-        userId
-        agency {
-          id
-          name
-          createdAt
-          updatedAt
-        }
-      }
+      success
     }
   }
 `;
