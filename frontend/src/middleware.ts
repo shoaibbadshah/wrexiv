2622577b86391377deb80fa845c372ef5e615b86
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-05-07 20:03:44 +0700

    feat: update middleware matcher rule

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -52,5 +52,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher:
-    "/((?!api/|_next/static|_next/image|favicon.ico).*$(?<!.jpg)(?<!.png)(?<!.jpeg)(?<!.svg))",
+    "/((?!api/|_next/static|_next/image|favicon.ico).*$(?<!.jpg|.png|.jpeg|.svg|.webp))",
 };
