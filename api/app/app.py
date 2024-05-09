345df54dc39f48f38a2161d65267869d672b1a23
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-05-09 16:45:14 +0700

    chore: rename app variables

--- api/app/app.py
+++ api/app/app.py
@@ -1,8 +1,8 @@
 from app import create_app
 
 
-flask_app = create_app()
-celery_app = flask_app.extensions["celery"]
+app = create_app()
+celery = app.extensions["celery"]
 
 if __name__ == "__main__":
-    flask_app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
