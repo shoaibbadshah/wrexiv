b857655e04c381cb5067a2621eab2a2cff72ede9
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-05-23 11:54:48 +0700

    refactor: change if-else to bool()

--- api/app/utilities/validation_utilities.py
+++ api/app/utilities/validation_utilities.py
@@ -5,6 +5,4 @@ def validate_email(email: str) -> bool:
     Validate email address
     """
     # Source: https://emailregex.com/
-    if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        return True
-    return False
+    return bool(re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email))
