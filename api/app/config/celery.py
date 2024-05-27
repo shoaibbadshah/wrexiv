1eb7e33bbbdb9a94ae01dfd904edd52ecbc85acf
Rozan Fadhil Al Hafidz <rozanfadhilalhafidz@gmail.com>
2024-05-27 21:44:54 +0700

    fix: dockerfile for celery and api; makefile

--- api/app/config/celery.py
+++ api/app/config/celery.py
@@ -1,14 +1,29 @@
 from celery import Celery, Task
 from flask import Flask
+import os
 
 def setup_celery(app: Flask) -> Celery:
-    app.config.from_mapping(
-        CELERY=dict(
-            broker_url="redis://redis",
-            result_backend="redis://redis",
-            task_ignore_result=True,
-        ),
-    )
+    flask_env = os.getenv("APP_ENV", "development")
+    if flask_env == "production":
+        redis_host = os.getenv("REDIS_HOST")
+        redis_password = os.getenv("REDIS_PASSWORD")
+        redis_url = f"redis://:{redis_password}@{redis_host}"
+        app.config.from_mapping(
+            CELERY=dict(
+                broker_url=redis_url,
+                result_backend=redis_url,
+                task_ignore_result=True,
+            ),
+        )
+    else:
+        app.config.from_mapping(
+            CELERY=dict(
+                broker_url="redis://redis",
+                result_backend="redis://redis",
+                task_ignore_result=True,
+            ),
+        )
+        
     class FlaskTask(Task):
         def __call__(self, *args: object, **kwargs: object) -> object:
             with app.app_context():
