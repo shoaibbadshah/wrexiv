0386a15fccfbb2afdb6313def15c46aff3e80697
Rozan Fadhil A <rozanfadhilalhafidz@gmail.com>
2024-06-12 19:20:18 +0700

    chore: comment out google_artifact_registry_repository

--- infra/cloud_resources/artifact_registry.tf
+++ infra/cloud_resources/artifact_registry.tf
@@ -1,6 +1,6 @@
-resource "google_artifact_registry_repository" "globaltalentdb" {
-  repository_id = "globaltalentdb"
-  location      = "asia-northeast1"
-  description   = "globaltalentdb repository"
-  format        = "DOCKER"
-}
+# resource "google_artifact_registry_repository" "globaltalentdb" {
+#   repository_id = "globaltalentdb"
+#   location      = "asia-northeast1"
+#   description   = "globaltalentdb repository"
+#   format        = "DOCKER"
+# }
